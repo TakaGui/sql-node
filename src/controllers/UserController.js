cd62e8c5720995b6b3e3607b8e1fa8b5056f54e5
TakaGui <gui.yukio175@gmail.com>
2020-09-14 21:10:05 -0300

    Create users

---
+++ src/controllers/UserController.js
@@ -0,0 +1,11 @@
+const User = require('../models/User');
+
+module.exports = {
+  async store(request, response) {
+    const { name, email } = request.body;
+
+    const user = await User.create({ name, email });
+
+    return response.json(user);
+  }
+}
