a0ff5e88e61dcba4ef0d36e2415fa5a05d2d066c
TakaGui <gui.yukio175@gmail.com>
2020-09-14 21:15:02 -0300

    List users

--- src/controllers/UserController.js
+++ src/controllers/UserController.js
@@ -1,6 +1,12 @@
 const User = require('../models/User');
 
 module.exports = {
+  async index(request, response) {
+    const users = await User.findAll();
+
+    return response.json(users);
+  },
+
   async store(request, response) {
     const { name, email } = request.body;
 
